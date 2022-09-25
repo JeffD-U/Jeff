b41bf66ab9e5461f7318101e731a4a29ed16c771
Jeff <65855990+JeffD-U@users.noreply.github.com>
2022-09-25 10:25:17 +0800

    Add files via upload

---
+++ eventObjects.js
@@ -0,0 +1,18 @@
+var data = [{
+    id: ,
+    title:'石门二路街道志愿者自治慈善超市',
+    briefintro,
+    gallery_url: [],
+},
+{
+    id: 1,
+    title:'',
+    briefintro,
+    gallery_url: []
+},
+{
+    id: 1,
+    title:'',
+    briefintro,
+    gallery_url: []
+}]
---
+++ projectObjects.js
@@ -0,0 +1,21 @@
+var data = [{
+    id: 1,
+    title:'石门二路街道志愿者自治慈善超市',
+    subtitle:'石门二路街道慈善超市，是一家完全由自愿者自治的慈善超市，我们的目标是成为一家“美好家园自治店”。',
+    Article_url: "",
+    image: "./image1.jpeg"
+},
+{
+    id: 1,
+    title:'',
+    subtitle:'',
+    Article_url: [],
+    image: []
+},
+{
+    id: 1,
+    title:'',
+    subtitle:'',
+    Article_url: [],
+    image: []
+}]
