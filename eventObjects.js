5cb5e5b2b0d4d8d421f4eaf63fa64cd833a2dd43
buy42.volunteer <65855990+JeffD-U@users.noreply.github.com>
2022-09-25 14:05:19 +0800

    Add files via upload

---
+++ eventObjects.js
@@ -0,0 +1,18 @@
+var evData = [{
+    id: 1,
+    title:'活动1',
+    image: './志愿者照片_16253 copy/IMG_20220128_141731.jpg',
+    gallery_url: [],
+},
+{
+    id: 2,
+    title:'活动2',
+    image: './志愿者照片_16253 copy/IMG_20220128_141731.jpg',
+    gallery_url: []
+},
+{
+    id: 3,
+    title:'活动3',
+    image: './志愿者照片_16253 copy/IMG_20220128_141731.jpg',
+    gallery_url: []
+}]
---
+++ projectObjects.js
@@ -0,0 +1,22 @@
+var projData = [{
+    id: '01',
+    title:'石门二路街道志愿者自治慈善超市',
+    briefintro: "石门二路街道慈善超市，是一家完全由自愿者自治的慈善超市，我们的目标是成为一家“美好家园自治店”。",
+    article: "/Users/jeff/Documents/Buy42 Web/石门二路街道志愿者自治慈善超市项目.html",
+    image: "./志愿者照片_16253notcaro/IMG_20210722_191117.jpg"
+},
+{
+    id: '02',
+    title:'项目2',
+    subtitle:'',
+    briefintro: "",
+    article: [],
+    image: "./志愿者照片_16253notcaro/IMG_20210722_191117.jpg"
+},
+{
+    id: '03',
+    title:'项目3',
+    briefintro: '',
+    article: [],
+    image: "./志愿者照片_16253notcaro/IMG_20210722_191117.jpg"
+}]
